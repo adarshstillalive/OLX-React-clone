27178a5dec5009b50c1a5b4291a413276d1d27f3
adarshstillalive <adarshstillalive@gmail.com>
2024-10-15 21:52:25 +0530

    Rm constant

--- src/utils/constant.tsx
+++
@@ -1,2 +0,0 @@
-export const LOGO_URL: string = "https://statics.olx.in/external/base/img/cartrade/logo/olx.svg?v=1";
-export const CATEGORIES = ["Cars", "Motorcycles", "Mobile Phones", "For Sale: Houses & Apartments", "Scooters", "Commercial & Other Vehicles", "For Rent: Houses & Apartments"];
